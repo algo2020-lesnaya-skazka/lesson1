d0ca1b86abb61b7a3869b9e1d4b029cc7af6171d
Yulia-Zoshchik <56930769+Yulia-Zoshchik@users.noreply.github.com>
2020-10-15 18:14:43 +0300

    Add files

---
+++ l14.py
@@ -0,0 +1,5 @@
+a = int(input(8))
+b = int(input(7))
+c = int(input(4))
+d = a + b + c 
+print(d)
---
+++ l15.py
@@ -0,0 +1,7 @@
+a = input() b = input() c = input()
+print("Имя:", end="")
+print(a)
+print("Фамилия:", end="")
+print(b)
+print("Телефон:", end="")
+print(c)
