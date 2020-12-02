dfd60d8be3748da39c0df51ebe769d54b6432966
Sasha331 <72512177+Sasha331@users.noreply.github.com>
2020-12-02 19:04:29 +0300

    Add task 1

---
+++ t1.py
@@ -0,0 +1,11 @@
+print("Введите первое число")
+a = int(input())
+print("Введите второе число")
+b = int(input())
+print("Ввудите третье число")
+c = int(input())
+print("Ввудите четвёртое число")
+d = int(input())
+e = (a-b)*c + d
+print("Результат равен")
+print(e)
