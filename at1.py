7a79d7d9bb1b2734596aa537cb438277c017b7b7
algo-lesnaya-skazka <56030516+algo-lesnaya-skazka@users.noreply.github.com>
2020-12-16 15:36:06 +0300

    Add files via upload

---
+++ at1.py
@@ -0,0 +1,12 @@
+print("Введите первое число")
+a = input()
+print("Введите второе число")
+b = input()
+print("Введите третие число")
+y = input()
+print("Введите четвёртое число")
+b = input()
+print("Вывод вычисления")
+print((int(a) - int(b)) * int(y) + int(b))
+
+
